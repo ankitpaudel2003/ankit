86a265d53afb1bec7a22f230512d5100b227844e
ankit <imankit.paudel@gmail.com>
2023-06-15 07:33:43 +0545

    first commit

---
+++ src/Pages/seaechbook.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function seaechbook() {
+  return (
+    <div>
+        <h1>SearchBook</h1>
+        <p>Search value</p>
+      
+    </div>
+  )
+}
+export default seaechbook
